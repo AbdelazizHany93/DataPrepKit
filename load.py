04f7c92660f90de9c99f30da22210a91ee63e464
AbdelazizHany93 <abdelazizhanyzaid@gmail.com>
2024-09-12 01:25:45 +0300

    load.py

--- load.py
+++ load.py
@@ -1,15 +1,34 @@
-#import libraries that i will use
 import pandas as pd
-import numpy as np
-## Display all the columns of the dataframe
-pd.pandas.set_option('display.max_columns',None)
 
-"""Read data from a CSV file."""
-def load_data(file_path,file_type):
-    if file_type=="csv":
-        data=pd.read_csv(file_path)
-    elif file_type=="xlsx":
-        data=pd.read_excel(file_path)
-    elif file_type=="json":
-        data=pd.read_json(file_path)
+def load_data(file_path, file_type):
+  """Loads data from a CSV, XLSX, or JSON file.
+
+  Args:
+    file_path (str): Path to the data file.
+    file_type (str): File extension (e.g., "csv", "xlsx", "json").
+
+  Returns:
+    pandas.DataFrame: Loaded data.
+
+  Raises:
+    FileNotFoundError: If the file path is incorrect or the file does not exist.
+    ValueError: If the file type is not supported.
+  """
+
+  try:
+    if file_type == "csv":
+      data = pd.read_csv(file_path)
+    elif file_type == "xlsx":
+      data = pd.read_excel(file_path)
+    elif file_type == "json":
+      data = pd.read_json(file_path)
+    else:
+      raise ValueError("Unsupported file type")
+
+    # Validate data types and ranges (if necessary)
+    # ...
+
     return data
+
+  except FileNotFoundError:
+    raise FileNotFoundError(f"File not found: {file_path}")
